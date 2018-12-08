d3285284c4c85031bdb1772de1cd10ff6c8a7def
Jimmy Smith <jaystansmith@gmail.com>
2018-12-08 15:45:44 -0700

    Implemented tests

--- app/controllers/oauth_connector_controller.rb
+++ app/controllers/oauth_connector_controller.rb
@@ -1,7 +1,8 @@
 class OauthConnectorController < ApplicationController
   def update
     data = request.env['omniauth.auth']
-    current_user.update(token: data.credentials.token)
+    user_token = data.credentials.token
+    current_user.update(token: user_token)
     redirect_to dashboard_path
   end
 end
