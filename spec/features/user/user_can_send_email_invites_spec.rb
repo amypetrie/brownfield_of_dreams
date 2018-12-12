736ab932d2f50cd83a1e0483e4dfd6d214df0865
Amy Leigh Petrie <apetrie9849@gmail.com>
2018-12-12 12:02:42 -0700

    fixes test file

--- spec/features/user/user_can_send_email_invites_spec.rb
+++ spec/features/user/user_can_send_email_invites_spec.rb
@@ -9,9 +9,6 @@ describe 'A registered user' do
     user = create(:user)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
     visit dashboard_path
-    save_and_open_page
-    click_on "Send an Invite"
-    expect(current_path).to eq new_invite_path
   end
 
 end
