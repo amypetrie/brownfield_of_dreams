c40631a4b80da42b967c6617a733bba55cf110f3
Amy Leigh Petrie <apetrie9849@gmail.com>
2018-12-13 15:27:32 -0700

    finishes functionality and completing texting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,7 @@ class UsersController < ApplicationController
 
   def update
     user = User.find(params[:user_id])
+    user.update(active: true)
     render :update
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_10_210339) do
+ActiveRecord::Schema.define(version: 2018_12_13_220916) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -79,6 +79,7 @@ ActiveRecord::Schema.define(version: 2018_12_10_210339) do
     t.datetime "updated_at", null: false
     t.string "token"
     t.string "uid"
+    t.boolean "active", default: false
     t.index ["email"], name: "index_users_on_email"
   end
 
